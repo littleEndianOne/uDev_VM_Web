274bb11017b81f205135e0aeef2311b2be1aaab3
David B <burgess.d@outlook.com>
2019-11-09 19:33:17 +0800

    Creating VM class

--- src/ts/vm_cpu.ts
+++ src/ts/vm_cpu.ts
@@ -1,13 +1,33 @@
 class Thread {
-    private _pc: Uint16Array
+    private _pc: Uint16Array;
 
-    get pc():Uint16Array {        
+    constructor() {
+
+    }
+
+    get pc(): Uint16Array {
         return this._pc;
     }
 
+}
+
+class VM {
+    public static newThread(): Thread {
+
+        return new Thread();
+    }
+
+    public static run(): void {
+
+    }
+
+    public static step(): void {
+
+    }
+
+    public static cpu(): void {
+
+    }
 
 }
 
-public newThread(): Thread {
-
-}
